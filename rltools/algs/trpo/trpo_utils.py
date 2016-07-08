66cba51944e39ef3809029527c2f543162a89777
Maxim Egorov <maxgegorov@gmail.com>
2016-07-07 21:23:24 -0700

    updated utils

--- rltools/algs/trpo/trpo_utils.py
+++ rltools/algs/trpo/trpo_utils.py
@@ -66,8 +66,8 @@ class VF(object):
         self.x = tf.placeholder(tf.float32, shape=[None, shape], name="x")
         self.y = tf.placeholder(tf.float32, shape=[None], name="y")
         self.net = (pt.wrap(self.x).
-                    fully_connected(128, activation_fn=tf.nn.relu).
-                    fully_connected(128, activation_fn=tf.nn.relu).
+                    fully_connected(64, activation_fn=tf.nn.relu).
+                    fully_connected(64, activation_fn=tf.nn.relu).
                     fully_connected(1))
         self.net = tf.reshape(self.net, (-1, ))
         l2 = (self.net - self.y) * (self.net - self.y)
--- rltools/utils/sims.py
+++ rltools/utils/sims.py
@@ -29,3 +29,21 @@ def evaluate(env, policy, maxsteps, n_traj, render=False):
         rewards[i] = rtot
     return rewards
 
+
+def evaluate_time(env, policy, maxsteps, n_traj):
+    rewards = np.zeros(n_traj)
+    times = np.zeros(n_traj)
+    for i in xrange(n_traj):
+        obs = env.reset()
+        t = 0
+        rtot = 0.0
+        for _ in xrange(maxsteps):
+            action, action_dist = policy.act(obs)
+            obs, reward, done, _ = env.step(action)
+            t += 1
+            rtot += reward
+            if done:
+                break
+        times[i] = t
+        rewards[i] = rtot
+    return rewards, times
