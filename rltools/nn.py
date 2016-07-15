2925bcf84a35dc709a4c9e04385a4e88e4cf2c16
rejuvyesh <mail@rejuvyesh.com>
2016-07-15 14:20:09 -0700

    Separate test from main file

--- rltools/nn.py
+++ rltools/nn.py
@@ -292,20 +292,3 @@ class Standardizer(Model):
         return y_B_D*(mu + self._eps) + mu
 
 
-def test_standardizer():
-    import numpy as np
-    D = 10
-    s = Standardizer(D, eps=0)
-
-    with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
-        x_N_D = np.random.randn(200, D)
-        s.update(sess, x_N_D)
-
-        x2_N_D = np.random.rand(300, D)
-        s.update(sess, x2_N_D)
-
-        allx = np.concatenate([x_N_D, x2_N_D], axis=0)
-        assert np.allclose(s.get_mean(sess)[0,:], allx.mean(axis=0))
-        assert np.allclose(s.get_stdev(sess)[0,:], allx.std(axis=0))
-        util.ok('standardizer ok')
---
+++ tests/test_standardizer.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+from __future__ import absolute_import
+
+import sys
+sys.path.append('.')
+import numpy as np
+
+import tensorflow as tf
+from rltools import nn
+
+def test_standardizer():
+    D = 10
+    s = nn.Standardizer(D, eps=0)
+
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+        x_N_D = np.random.randn(200, D)
+        s.update(sess, x_N_D)
+
+        x2_N_D = np.random.rand(300, D)
+        s.update(sess, x2_N_D)
+
+        allx = np.concatenate([x_N_D, x2_N_D], axis=0)
+
+        assert np.allclose(s.get_mean(sess)[0,:], allx.mean(axis=0))
+        assert np.allclose(s.get_stdev(sess)[0,:], allx.std(axis=0))
