f68f4c7d3604c5d7b59324b0235731e04aa19f7f
rejuvyesh <mail@rejuvyesh.com>
2016-07-17 00:27:56 -0700

    Fix sample script

--- simple_trpo_example.py
+++ simple_trpo_example.py
@@ -10,19 +10,18 @@ from __future__ import absolute_import, print_function
 
 import argparse
 import json
-import sys
 
-import numpy as np
 import tensorflow as tf
 
 import gym
-import rltools.algos
+import rltools.algos.policyopt
 import rltools.log
 import rltools.util
-from rltools.sampler import SimpleSampler, ImportanceWeightedSampler, DecSampler
-from rltools.baseline import LinearFeatureBaseline, MLPBaseline, ZeroBaseline
-from rltools.categorical_policy import CategoricalMLPPolicy
-
+from rltools.samplers.serial import SimpleSampler, ImportanceWeightedSampler, DecSampler
+from rltools.baselines.linear import LinearFeatureBaseline
+from rltools.baselines.mlp import MLPBaseline
+from rltools.baselines.zero import ZeroBaseline
+from rltools.policy.categorical import CategoricalMLPPolicy
 
 SIMPLE_POLICY_ARCH = '''[
         {"type": "fc", "n": 32},
@@ -42,27 +41,37 @@ SIMPLE_VAL_ARCH = '''[
 
 def main():
     parser = argparse.ArgumentParser()
-    
-    parser.add_argument('--env', type=str, default='CartPole-v0') # gym environment
 
-    parser.add_argument('--discount', type=float, default=0.95) 
+    parser.add_argument('--env', type=str,
+                        default='CartPole-v0')  # gym environment
+
+    parser.add_argument('--discount', type=float, default=0.95)
     parser.add_argument('--gae_lambda', type=float, default=0.99)
 
-    parser.add_argument('--n_iter', type=int, default=250) # trpo iterations
-    parser.add_argument('--max_traj_len', type=int, default=200) # max length of a trajectory (ts)
-    parser.add_argument('--batch_size', type=int, default=32) # number of traj in an iteration
+    parser.add_argument('--n_iter', type=int, default=250)  # trpo iterations
+    parser.add_argument('--max_traj_len',
+                        type=int,
+                        default=200)  # max length of a trajectory (ts)
+    parser.add_argument('--batch_size', type=int,
+                        default=32)  # number of traj in an iteration
 
-    parser.add_argument('--policy_hidden_spec', type=str, default=SIMPLE_POLICY_ARCH) # policy net architecture
-    parser.add_argument('--baseline_hidden_spec', type=str, default=SIMPLE_VAL_ARCH) # baseline value net architecture
+    parser.add_argument('--policy_hidden_spec',
+                        type=str,
+                        default=SIMPLE_POLICY_ARCH)  # policy net architecture
+    parser.add_argument(
+        '--baseline_hidden_spec',
+        type=str, default=SIMPLE_VAL_ARCH)  # baseline value net architecture
 
     # TRPO params
     parser.add_argument('--max_kl', type=float, default=0.01)
     parser.add_argument('--vf_max_kl', type=float, default=0.01)
     parser.add_argument('--vf_cg_damping', type=float, default=0.01)
 
-    parser.add_argument('--save_freq', type=int, default=20) # how often checkpoints are made
-    parser.add_argument('--log', type=str, required=False) # log file
-    parser.add_argument('--tblog', type=str, default='/tmp/madrl_tb') # tensorboard log dir
+    parser.add_argument('--save_freq', type=int,
+                        default=20)  # how often checkpoints are made
+    parser.add_argument('--log', type=str, required=False)  # log file
+    parser.add_argument('--tblog', type=str,
+                        default='/tmp/madrl_tb')  # tensorboard log dir
 
     args = parser.parse_args()
 
@@ -70,15 +79,22 @@ def main():
     env = gym.make(args.env)
 
     # init policy network
-    policy = CategoricalMLPPolicy(env.observation_space, env.action_space,
+    policy = CategoricalMLPPolicy(env.observation_space,
+                                  env.action_space,
                                   hidden_spec=args.policy_hidden_spec,
-                                  enable_obsnorm=True, 
-                                  tblog=args.tblog, varscope_name='pursuit_catmlp_policy')
+                                  enable_obsnorm=True,
+                                  tblog=args.tblog,
+                                  varscope_name='pursuit_catmlp_policy')
 
     # init baseline
-    baseline = MLPBaseline(env.observation_space, args.baseline_hidden_spec,
-                           True, True, max_kl=args.vf_max_kl, damping=args.vf_cg_damping,
-                           time_scale=1./args.max_traj_len, varscope_name='pursuit_mlp_baseline')
+    baseline = MLPBaseline(env.observation_space,
+                           args.baseline_hidden_spec,
+                           True,
+                           True,
+                           max_kl=args.vf_max_kl,
+                           damping=args.vf_cg_damping,
+                           time_scale=1. / args.max_traj_len,
+                           varscope_name='pursuit_mlp_baseline')
 
     # init sampler
     sampler_cls = SimpleSampler
@@ -90,10 +106,10 @@ def main():
                         adaptive=False)
 
     # init TRPO step function
-    step_func = rltools.algos.TRPO(max_kl=args.max_kl)
+    step_func = rltools.algos.policyopt.TRPO(max_kl=args.max_kl)
 
     # init optimizer
-    popt = rltools.algos.SamplingPolicyOptimizer(
+    popt = rltools.algos.policyopt.SamplingPolicyOptimizer(
         env=env,
         policy=policy,
         baseline=baseline,
@@ -102,8 +118,7 @@ def main():
         gae_lambda=args.gae_lambda,
         sampler_cls=sampler_cls,
         sampler_args=sampler_args,
-        n_iter=args.n_iter
-    )
+        n_iter=args.n_iter)
 
     # initialize logger
     argstr = json.dumps(vars(args), separators=(',', ':'), indent=2)
