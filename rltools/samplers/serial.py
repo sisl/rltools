f2eeb9ae2ea12f530c68d202633175347df499ee
rejuvyesh <mail@rejuvyesh.com>
2016-07-16 17:08:11 -0700

    Refactor sampler

--- rltools/samplers/serial.py
+++ rltools/samplers/serial.py
@@ -1,209 +1,7 @@
-import copy
-import random
-
 import numpy as np
 
-import util
-
-
-class Trajectory(object):
-    __slots__ = ('obs_T_Do', 'obsfeat_T_Df', 'adist_T_Pa', 'a_T_Da', 'r_T')
-    def __init__(self, obs_T_Do, obsfeat_T_Df, adist_T_Pa, a_T_Da, r_T):
-        assert (
-            obs_T_Do.ndim > 1 and obsfeat_T_Df.ndim > 1 and adist_T_Pa.ndim > 1 and a_T_Da.ndim > 1 and
-            r_T.ndim == 1 and
-            obs_T_Do.shape[0] == obsfeat_T_Df.shape[0] == adist_T_Pa.shape[0] == a_T_Da.shape[0] == r_T.shape[0]
-        )
-        self.obs_T_Do = obs_T_Do
-        self.obsfeat_T_Df = obsfeat_T_Df
-        self.adist_T_Pa = adist_T_Pa
-        self.a_T_Da = a_T_Da
-        self.r_T = r_T
-
-    def __len__(self):
-        return self.obs_T_Do.shape[0]
-
-    # Saving/loading discards obsfeat
-    def save_h5(self, grp, **kwargs):
-        grp.create_dataset('obs_T_Do', data=self.obs_T_Do, **kwargs)
-        grp.create_dataset('adist_T_Pa', data=self.adist_T_Pa, **kwargs)
-        grp.create_dataset('a_T_Da', data=self.a_T_Da, **kwargs)
-        grp.create_dataset('r_T', data=self.r_T, **kwargs)
-
-    @classmethod
-    def LoadH5(cls, grp, obsfeat_fn):
-        """
-        obsfeat_fn: Used to fill in observation features.
-                    If None, the raw observations will be copied over.
-        """
-        obs_T_Do = grp['obs_T_Do'][...]
-        obsfeat_T_Df = obsfeat_fn(obs_T_Do) if obsfeat_fn is not None else obs_T_Do.copy()
-        return cls(obs_T_Do, obsfeat_T_Df, grp['adist_T_Pa'][...], grp['a_T_Da'][...], grp['r_T'][...])
-
-
-def raggedstack(arrays, fill=0., axis=0, raggedaxis=1):
-    """
-    Stacks a list of arrays, like np.stack with axis=0.
-    Arrays may have different length (along the raggedaxis), and will be padded on the right
-    with the given fill value.
-    """
-    assert axis == 0 and raggedaxis == 1, 'not implemented'
-    arrays = [a[None,...] for a in arrays]
-    assert all(a.ndim >= 2 for a in arrays)
-
-    outshape = list(arrays[0].shape)
-    outshape[0] = sum(a.shape[0] for a in arrays)
-    outshape[1] = max(a.shape[1] for a in arrays) # take max along ragged axes
-    outshape = tuple(outshape)
-
-    out = np.full(outshape, fill, dtype=arrays[0].dtype)
-    pos = 0
-    for a in arrays:
-        out[pos:pos+a.shape[0], :a.shape[1], ...] = a
-        pos += a.shape[0]
-    assert pos == out.shape[0]
-    return out
-
-
-class RaggedArray(object):
-    def __init__(self, arrays, lengths=None):
-        if lengths is None:
-            # Without provided lengths, `arrays` is interpreted as a list of arrays
-            # and self.lengths is set to the list of lengths for those arrays
-            self.arrays = arrays
-            self.stacked = np.concatenate(arrays, axis=0)
-            self.lengths = np.array([len(a) for a in arrays])
-        else:
-            # With provided lengths, `arrays` is interpreted as concatenated data
-            # and self.lengths is set to the provided lengths.
-            self.arrays = np.split(arrays, np.cumsum(lengths)[:-1])
-            self.stacked = arrays
-            self.lengths = np.asarray(lengths, dtype=int)
-            assert all(len(a) == l for a,l in util.safezip(self.arrays, self.lengths))
-            self.boundaries = np.concatenate([[0], np.cumsum(self.lengths)])
-            assert self.boundaries[-1] == len(self.stacked)
-    def __len__(self):
-        return len(self.lengths)
-    def __getitem__(self, idx):
-        return self.stacked[self.boundaries[idx]:self.boundaries[idx+1], ...]
-    def padded(self, fill=0.):
-        return raggedstack(self.arrays, fill=fill, axis=0, raggedaxis=1)
-
-
-class TrajBatch(object):
-    def __init__(self, trajs, obs, obsfeat, adist, a, r, time):
-        self.trajs, self.obs, self.obsfeat, self.adist, self.a, self.r, self.time = trajs, obs, obsfeat, adist, a, r, time
-
-    @classmethod
-    def FromTrajs(cls, trajs):
-        assert all(isinstance(traj, Trajectory) for traj in trajs)
-        obs = RaggedArray([t.obs_T_Do for t in trajs])
-        obsfeat = RaggedArray([t.obsfeat_T_Df for t in trajs])
-        adist = RaggedArray([t.adist_T_Pa for t in trajs])
-        a = RaggedArray([t.a_T_Da for t in trajs])
-        r = RaggedArray([t.r_T for t in trajs])
-        time = RaggedArray([np.arange(len(t), dtype=float) for t in trajs])
-        return cls(trajs, obs, obsfeat, adist, a, r, time)
-
-    def with_replaced_reward(self, new_r):
-        new_trajs = [Trajectory(traj.obs_T_Do, traj.obsfeat_T_Df, traj.adist_T_Pa, traj.a_T_Da, traj_new_r) for traj, traj_new_r in util.safezip(self.trajs, new_r)]
-        return TrajBatch(new_trajs, self.obs, self.obsfeat, self.adist, self.a, new_r, self.time)
-
-    def with_replaced_adist(self, new_adist):
-        new_trajs = [Trajectory(traj.obs_T_Do, traj.obsfeat_T_Df, traj_new_adist, traj.a_T_Da, traj.r_T) for traj, traj_new_adist in util.safezip(self.trajs, new_adist)]
-        return TrajBatch(new_trajs, self.obs, self.obsfeat, new_adist, self.a, self.r, self.time)
-
-    def __len__(self):
-        return len(self.trajs)
-
-    def __getitem__(self, idx):
-        return self.trajs[idx]
-
-    def save_h5(self, f, starting_id=0, **kwargs):
-        for i, traj in enumerate(self.trajs):
-            traj.save_h5(f.require_group('%06d' % (i+starting_id)), **kwargs)
-
-    @classmethod
-    def LoadH5(cls, dset, obsfeat_fn):
-        return cls.FromTrajs([Trajectory.LoadH5(v, obsfeat_fn) for k, v in dset.iteritems()])
-
-
-class Sampler(object):
-    """
-    Base Sampler class
-    """
-    def __init__(self, algo, max_traj_len, batch_size, min_batch_size, max_batch_size, batch_rate, adaptive):
-        self.algo = algo
-        self.max_traj_len = max_traj_len
-        self.adaptive = adaptive
-        self.batch_size = batch_size
-        if self.adaptive:
-            self.batch_size = min_batch_size
-        self.min_batch_size = min_batch_size
-        self.max_batch_size = max_batch_size
-        self.batch_rate = batch_rate
-
-
-    def start(self):
-        """Init sampler"""
-        raise NotImplementedError()
-
-    def sample(self, itr):
-        """Collect samples"""
-        raise NotImplementedError()
-
-    def process(self, sess, itr, trajbatch):
-        assert len(trajbatch) == self.batch_size
-        trajlens = [len(traj) for traj in trajbatch]
-        maxT = max(trajlens)
-
-        rewards_B_T = trajbatch.r.padded(fill=0.)
-        qvals_zfilled_B_T = util.discount(rewards_B_T, self.algo.discount); assert qvals_zfilled_B_T.shape == (self.batch_size, maxT)
-        q = RaggedArray([qvals_zfilled_B_T[i,:len(traj)] for i, traj in enumerate(trajbatch)])
-        q_B_T = q.padded(fill=np.nan) # q vals padded with nans in the end
-        assert q_B_T.shape == (self.batch_size, maxT)
-
-        # Time-dependent baseline
-        simplev_B_T = np.tile(np.nanmean(q_B_T, axis=0, keepdims=True), (self.batch_size, 1)); assert simplev_B_T.shape == (self.batch_size, maxT)
-        simplev = RaggedArray([simplev_B_T[i,:len(traj)] for i, traj in enumerate(trajbatch)])
-
-        # State-dependent baseline
-        v_stacked = self.algo.baseline.predict(sess, trajbatch); assert v_stacked.ndim == 1
-        v = RaggedArray(v_stacked, lengths=trajlens)
-
-        # Compare squared loss of value function to that of time-dependent value function
-        # Explained variance
-        # *_r2 = 1 - var(y-ypred)/var(y)
-        # *_r2 = 0 => Useless
-        # *_r2 = 1 => Perfect
-        # *_r2 <0 => Worse than useless
-        constfunc_prediction_loss = np.var(q.stacked)
-        simplev_prediction_loss = np.var(q.stacked-simplev.stacked)
-        simplev_r2 = 1. - simplev_prediction_loss/(constfunc_prediction_loss + 1e-8)
-        vfunc_prediction_loss = np.var(q.stacked-v_stacked)
-        vfunc_r2 = 1. - vfunc_prediction_loss/(constfunc_prediction_loss + 1e-8)
-
-        # XXX HACK
-        if vfunc_r2 < 0:
-            v = simplev
-
-        # Compute advantage -- GAE(gamma,lambda) estimator
-        v_B_T = v.padded(fill=0.)
-        v_B_Tp1 = np.concatenate([v_B_T, np.zeros((self.batch_size,1))], axis=1); assert v_B_Tp1.shape == (self.batch_size, maxT+1)
-        delta_B_T = rewards_B_T + self.algo.discount*v_B_Tp1[:,1:] - v_B_Tp1[:,:-1]
-        adv_B_T = util.discount(delta_B_T, self.algo.discount*self.algo.gae_lambda); assert adv_B_T.shape == (self.batch_size, maxT)
-        adv = RaggedArray([adv_B_T[i,:l] for i,l in enumerate(trajlens)])
-        assert np.allclose(adv.padded(fill=0), adv_B_T)
-
-        # Fit for the next time step
-        baseline_info = self.algo.baseline.fit(sess, trajbatch, q.stacked)
-
-        return dict(advantage=adv, qval=q, v_r=vfunc_r2, tv_r=simplev_r2), baseline_info
-
-
-    def stop(self):
-        raise NotImplementedError()
-
+from rltools.samplers import Sampler
+from rltools.trajutil import TrajBatch, Trajectory
 
 
 class SimpleSampler(Sampler):
@@ -257,17 +55,17 @@ class DecSampler(Sampler):
                 self.batch_size *= 2
         def get_lists(nl, na):
             l = []
-            for i in xrange(nl):
+            for i in range(nl):
                 l.append([[] for j in xrange(na)])
             return l
 
         env = self.algo.env
         trajs = []
-        for _ in xrange(self.batch_size / env.n_agents()): #FIXME: batch size depends on number of agents
+        for _ in range(self.batch_size / env.n_agents()): #FIXME: batch size depends on number of agents
             old_ob = env.reset()
             n_total = env.n_agents()
             obs, obsfeat, actions, actiondists, rewards = get_lists(5, n_total)
-            for itr in xrange(self.max_traj_len):
+            for itr in range(self.max_traj_len):
                 agent_actions = []
                 n = env.n_agents()
                 for i, agent_obs in enumerate(old_ob):
@@ -278,15 +76,15 @@ class DecSampler(Sampler):
                     agent_actions.append(a)
                     actions[i].append(a)
                     actiondists[i].append(adist)
-                new_ob, r, done, _ = env.step(np.array(agent_actions)[:,0,0]) #FIXME
+                    new_ob, r, done, _ = env.step(np.array(agent_actions)[:,0,0]) #FIXME
                 for i, o in enumerate(old_ob):
                     if o is None: continue
                     rewards[i].append(r)
-                old_ob = new_ob
+                    old_ob = new_ob
                 if done:
                     break
 
-            for agnt in xrange(n_total):
+            for agnt in range(n_total):
                 obs_T_Do = np.concatenate(obs[agnt])
                 obsfeat_T_Df = np.concatenate(obsfeat[agnt])
                 adist_T_Pa = np.concatenate(actiondists[agnt])
@@ -300,20 +98,6 @@ class DecSampler(Sampler):
                  ('avglen', int(np.mean([len(traj) for traj in trajbatch])), int), # average traj length
                  ('ravg', trajbatch.r.stacked.mean(), int) # avg reward encountered per time step (probably not that useful)
                 ])
-
-
-
-class BatchSampler(Sampler):
-    def __init__(self, algo):
-        self.algo = algo
-
-    def sample(self, itr):
-        # Completed trajs
-        num_sa = 0
-        completed_trajlists = []
-
-        # Simulations and their current trajectories
-        # TODO
 
 
 class ImportanceWeightedSampler(SimpleSampler):
@@ -413,10 +197,10 @@ class ImportanceWeightedSampler(SimpleSampler):
                 traj.r_T *= is_ratio
 
             rettrajs.extend(samples)
-        # Pack them back
+            # Pack them back
         if len(rettrajs) > self.batch_size:
             rettrajs = random.sample(rettrajs, self.batch_size)
-        rettrajbatch = TrajBatch.FromTrajs(rettrajs)
+            rettrajbatch = TrajBatch.FromTrajs(rettrajs)
 
         batch_info = [('ret', rettrajbatch.r.padded(fill=0.).sum(axis=1).mean(), float), # average return for batch of traj
                       ('avglen', int(np.mean([len(traj) for traj in rettrajbatch])), int), # average traj length
---
+++ rltools/trajutil.py
@@ -0,0 +1,128 @@
+import copy
+import random
+
+import numpy as np
+
+from rltools import util
+
+
+class Trajectory(object):
+    __slots__ = ('obs_T_Do', 'obsfeat_T_Df', 'adist_T_Pa', 'a_T_Da', 'r_T')
+    def __init__(self, obs_T_Do, obsfeat_T_Df, adist_T_Pa, a_T_Da, r_T):
+        assert (
+            obs_T_Do.ndim > 1 and obsfeat_T_Df.ndim > 1 and adist_T_Pa.ndim > 1 and a_T_Da.ndim > 1 and
+            r_T.ndim == 1 and
+            obs_T_Do.shape[0] == obsfeat_T_Df.shape[0] == adist_T_Pa.shape[0] == a_T_Da.shape[0] == r_T.shape[0]
+        )
+        self.obs_T_Do = obs_T_Do
+        self.obsfeat_T_Df = obsfeat_T_Df
+        self.adist_T_Pa = adist_T_Pa
+        self.a_T_Da = a_T_Da
+        self.r_T = r_T
+
+    def __len__(self):
+        return self.obs_T_Do.shape[0]
+
+    # Saving/loading discards obsfeat
+    def save_h5(self, grp, **kwargs):
+        grp.create_dataset('obs_T_Do', data=self.obs_T_Do, **kwargs)
+        grp.create_dataset('adist_T_Pa', data=self.adist_T_Pa, **kwargs)
+        grp.create_dataset('a_T_Da', data=self.a_T_Da, **kwargs)
+        grp.create_dataset('r_T', data=self.r_T, **kwargs)
+
+    @classmethod
+    def LoadH5(cls, grp, obsfeat_fn):
+        """
+        obsfeat_fn: Used to fill in observation features.
+                    If None, the raw observations will be copied over.
+        """
+        obs_T_Do = grp['obs_T_Do'][...]
+        obsfeat_T_Df = obsfeat_fn(obs_T_Do) if obsfeat_fn is not None else obs_T_Do.copy()
+        return cls(obs_T_Do, obsfeat_T_Df, grp['adist_T_Pa'][...], grp['a_T_Da'][...], grp['r_T'][...])
+
+
+def raggedstack(arrays, fill=0., axis=0, raggedaxis=1):
+    """
+    Stacks a list of arrays, like np.stack with axis=0.
+    Arrays may have different length (along the raggedaxis), and will be padded on the right
+    with the given fill value.
+    """
+    assert axis == 0 and raggedaxis == 1, 'not implemented'
+    arrays = [a[None,...] for a in arrays]
+    assert all(a.ndim >= 2 for a in arrays)
+
+    outshape = list(arrays[0].shape)
+    outshape[0] = sum(a.shape[0] for a in arrays)
+    outshape[1] = max(a.shape[1] for a in arrays) # take max along ragged axes
+    outshape = tuple(outshape)
+
+    out = np.full(outshape, fill, dtype=arrays[0].dtype)
+    pos = 0
+    for a in arrays:
+        out[pos:pos+a.shape[0], :a.shape[1], ...] = a
+        pos += a.shape[0]
+    assert pos == out.shape[0]
+    return out
+
+
+class RaggedArray(object):
+    def __init__(self, arrays, lengths=None):
+        if lengths is None:
+            # Without provided lengths, `arrays` is interpreted as a list of arrays
+            # and self.lengths is set to the list of lengths for those arrays
+            self.arrays = arrays
+            self.stacked = np.concatenate(arrays, axis=0)
+            self.lengths = np.array([len(a) for a in arrays])
+        else:
+            # With provided lengths, `arrays` is interpreted as concatenated data
+            # and self.lengths is set to the provided lengths.
+            self.arrays = np.split(arrays, np.cumsum(lengths)[:-1])
+            self.stacked = arrays
+            self.lengths = np.asarray(lengths, dtype=int)
+            assert all(len(a) == l for a,l in util.safezip(self.arrays, self.lengths))
+            self.boundaries = np.concatenate([[0], np.cumsum(self.lengths)])
+            assert self.boundaries[-1] == len(self.stacked)
+    def __len__(self):
+        return len(self.lengths)
+    def __getitem__(self, idx):
+        return self.stacked[self.boundaries[idx]:self.boundaries[idx+1], ...]
+    def padded(self, fill=0.):
+        return raggedstack(self.arrays, fill=fill, axis=0, raggedaxis=1)
+
+
+class TrajBatch(object):
+    def __init__(self, trajs, obs, obsfeat, adist, a, r, time):
+        self.trajs, self.obs, self.obsfeat, self.adist, self.a, self.r, self.time = trajs, obs, obsfeat, adist, a, r, time
+
+    @classmethod
+    def FromTrajs(cls, trajs):
+        assert all(isinstance(traj, Trajectory) for traj in trajs)
+        obs = RaggedArray([t.obs_T_Do for t in trajs])
+        obsfeat = RaggedArray([t.obsfeat_T_Df for t in trajs])
+        adist = RaggedArray([t.adist_T_Pa for t in trajs])
+        a = RaggedArray([t.a_T_Da for t in trajs])
+        r = RaggedArray([t.r_T for t in trajs])
+        time = RaggedArray([np.arange(len(t), dtype=float) for t in trajs])
+        return cls(trajs, obs, obsfeat, adist, a, r, time)
+
+    def with_replaced_reward(self, new_r):
+        new_trajs = [Trajectory(traj.obs_T_Do, traj.obsfeat_T_Df, traj.adist_T_Pa, traj.a_T_Da, traj_new_r) for traj, traj_new_r in util.safezip(self.trajs, new_r)]
+        return TrajBatch(new_trajs, self.obs, self.obsfeat, self.adist, self.a, new_r, self.time)
+
+    def with_replaced_adist(self, new_adist):
+        new_trajs = [Trajectory(traj.obs_T_Do, traj.obsfeat_T_Df, traj_new_adist, traj.a_T_Da, traj.r_T) for traj, traj_new_adist in util.safezip(self.trajs, new_adist)]
+        return TrajBatch(new_trajs, self.obs, self.obsfeat, new_adist, self.a, self.r, self.time)
+
+    def __len__(self):
+        return len(self.trajs)
+
+    def __getitem__(self, idx):
+        return self.trajs[idx]
+
+    def save_h5(self, f, starting_id=0, **kwargs):
+        for i, traj in enumerate(self.trajs):
+            traj.save_h5(f.require_group('%06d' % (i+starting_id)), **kwargs)
+
+    @classmethod
+    def LoadH5(cls, dset, obsfeat_fn):
+        return cls.FromTrajs([Trajectory.LoadH5(v, obsfeat_fn) for k, v in dset.iteritems()])
