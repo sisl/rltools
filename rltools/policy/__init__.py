25665685f87d7fbab6d60fb4bbb2733fc23ac955
rejuvyesh <mail@rejuvyesh.com>
2016-08-31 12:12:10 -0700

    accept kwargs

--- rltools/policy/__init__.py
+++ rltools/policy/__init__.py
@@ -7,7 +7,7 @@ class Policy(nn.Model):
         self._observation_space = observation_space
         self._action_space = action_space
 
-    def reset(self, *args):
+    def reset(self, *args, **kwargs):
         pass
 
     @property
